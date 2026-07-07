37a0f71df5b2175e84d13958beec05b20ab1e803
Fiber Label <fiberlabelace@gmail.com>
2026-07-07 11:34:44 +0700

    fix logo

--- src/movie/index.jsx
+++ src/movie/index.jsx
@@ -62,7 +62,7 @@ useEffect(() => {
         <nav className="navbar navbar-expand-lg">
           <div className="container">
             <a className="navbar-brand" href="#">
-              <img src="./public/cgvlogo.png" alt="CGV" height="40"/>
+              <img src="/cgvlogo.png" alt="CGV" height="40"/>
             </a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
